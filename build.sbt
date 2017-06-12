9d5a5b536c7168c5bd7e143275612c838830595b
Keith Cissell <kcissell@keith-cis-mac.local>
2017-06-12 16:39:04 -0500

    Working on Get and Post methods

--- build.sbt
+++ build.sbt
@@ -2,5 +2,6 @@ name := "specs2-tutorial"
 
 libraryDependencies ++= Seq(
   "org.specs2" %% "specs2-core" % "3.7.2" % "test",
-  "org.asynchttpclient" % "async-http-client" % "2.0.32"
+  "org.asynchttpclient" % "async-http-client" % "2.0.32",
+  "org.slf4j" % "slf4j-simple" % "1.7.21" % "runtime"
 )
--- src/main/scala/modulework/HTTP-Client/Http-Practice.scala
+++ src/main/scala/modulework/HTTP-Client/Http-Practice.scala
@@ -1,19 +1,40 @@
 // src/main/scala/modulework/HTTP-Client/Http-Practice.scala
 package httpclient
 import org.asynchttpclient._
+import java.util.concurrent.Future
 
-object HttpPractice {
+case class HttpResponse(header: String, body: String, statusCode: Int)
 
-  case class HttpResponse(header: String, body: String, statusCode: Int)
+trait HttpClient {
+  def executeHttpPost(url: String, body: Map[String,String]): HttpResponse
+  def executeHttpGet(url: String): HttpResponse
+}
 
-  trait HttpClient {
-    def executeHttpPost(url: String, body: Map[String,String]): HttpResponse
-    def executeHttpGet(url: String): HttpResponse
+class API extends HttpClient {
+
+  def executeHttpPost(url: String, body: Map[String,String]): HttpResponse = {
+    val asyncHttpClient = new DefaultAsyncHttpClient()
+    val f = asyncHttpClient.preparePost(s"$url").setBody(body.toString).execute()
+    val r = f.get()
+    asyncHttpClient.close()
+    HttpResponse(r.getContentType(), r.getResponseBody(), r.getStatusCode())
   }
 
+  def executeHttpGet(url: String): HttpResponse = {
+    val asyncHttpClient = new DefaultAsyncHttpClient()
+    val f = asyncHttpClient.prepareGet(s"$url").execute()
+    val r = f.get()
+    asyncHttpClient.close()
+    HttpResponse(r.getContentType(), r.getResponseBody(), r.getStatusCode())
+  }
+}
 
-
+object HttpPractice {
   def main(args: Array[String]) {
-
+    val testAPI = new API
+    val getResponse = testAPI.executeHttpGet("https://www.google.com/")
+    val postResponse = testAPI.executeHttpPost("https://www.google.com/", Map("message" -> "hello world"))
+    println(getResponse.statusCode)
+    println(postResponse.statusCode)
   }
 }
