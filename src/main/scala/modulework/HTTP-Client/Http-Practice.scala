90682d27c2b1079485fb8752c2796164c8161636
Keith Cissell <kcissell@keith-cis-mac.local>
2017-06-13 11:42:37 -0500

    Completed practice with Http client

--- src/main/scala/modulework/HTTP-Client/Http-Practice.scala
+++ src/main/scala/modulework/HTTP-Client/Http-Practice.scala
@@ -2,8 +2,9 @@
 package httpclient
 import org.asynchttpclient._
 import java.util.concurrent.Future
+import scala.collection.JavaConversions._
 
-case class HttpResponse(header: String, body: String, statusCode: Int)
+case class HttpResponse(header: Map[String,String], body: String, statusCode: Int)
 
 trait HttpClient {
   def executeHttpPost(url: String, body: Map[String,String]): HttpResponse
@@ -13,30 +14,51 @@ trait HttpClient {
 class API extends HttpClient {
 
   def executeHttpPost(url: String, body: Map[String,String]): HttpResponse = {
+    // Make request
     val asyncHttpClient = new DefaultAsyncHttpClient()
     val req = asyncHttpClient.preparePost(s"$url")
     for ((n,v) <- body) req.addFormParam(n, v)
     val f = req.execute()
     val r = f.get()
     asyncHttpClient.close()
-    HttpResponse(r.getContentType(), r.getResponseBody(), r.getStatusCode())
+
+    // Build custom HttpResponse
+    val hMap: Map[String, String] = if (r.hasResponseHeaders()) {
+      val names = r.getHeaders().names().toList
+      val values = names.map(n => r.getHeaders().get(n))
+      (names zip values).toMap
+    } else Map()
+    val b = if (r.hasResponseBody()) r.getResponseBody() else ""
+    val s = if (r.hasResponseStatus()) r.getStatusCode() else 0
+    HttpResponse(hMap, b, s)
   }
 
   def executeHttpGet(url: String): HttpResponse = {
+    // Make request
     val asyncHttpClient = new DefaultAsyncHttpClient()
     val f = asyncHttpClient.prepareGet(s"$url").execute()
     val r = f.get()
     asyncHttpClient.close()
-    HttpResponse(r.getContentType(), r.getResponseBody(), r.getStatusCode())
+
+    // Build custom HttpResponse
+    val hMap: Map[String, String] = if (r.hasResponseHeaders()) {
+      val names = r.getHeaders().names().toList
+      val values = names.map(n => r.getHeaders().get(n))
+      (names zip values).toMap
+    } else Map()
+    val b = if (r.hasResponseBody()) r.getResponseBody() else ""
+    val s = if (r.hasResponseStatus()) r.getStatusCode() else 0
+    HttpResponse(hMap, b, s)
   }
 }
 
 object HttpPractice {
   def main(args: Array[String]) {
+    val myMap = Map("message" -> "hello", "from" -> "keith", "to" -> "world")
     val testAPI = new API
-    val getResponse = testAPI.executeHttpGet("https://httpbin.org/get")
-    val postResponse = testAPI.executeHttpPost("https://httpbin.org/post", Map("message" -> "hello world"))
-    println(getResponse.body)
-    println(postResponse.body)
+    val getR = testAPI.executeHttpGet("https://httpbin.org/get")
+    val postR = testAPI.executeHttpPost("https://httpbin.org/post", myMap)
+    println(getR.header + "\n" + getR.body + "\n" + getR.statusCode)
+    println(postR.header + "\n" + postR.body + "\n" + postR.statusCode)
   }
 }
